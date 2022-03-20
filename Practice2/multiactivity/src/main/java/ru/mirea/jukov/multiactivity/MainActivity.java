fc26ed189be89bcb29eb36f86613dd92419fdbc5
OloV03 <jukov46@yandex.ru>
2022-03-20 21:27:30 +0300

    practice #2

---
+++ Practice2/multiactivity/src/main/java/ru/mirea/jukov/multiactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package ru.mirea.jukov.multiactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickNewActivity(View view) {
+        Intent intent  = new Intent(this, SecondActivity.class);
+        intent.putExtra("key", "MIREA - Жуков Илья Владимирович");
+        startActivity(intent);
+    }
+}
